c59a81ad09ca3e911adbfe1e79c432cac9f1e5f1
brayangamezt <77857832+brayangamezt@users.noreply.github.com>
2023-07-16 22:14:45 -0700

    update routes again

--- src/routes/FirstScreen.jsx
+++ src/routes/FirstScreen.jsx
@@ -10,7 +10,7 @@ const FirstScreen = () => {
         <div className='glass-container max-[500px]:w-3/4'>
           <img className='w-3/4 h-3/4 max-[500px]:w-4/5 drop-shadow animate-[logoAnimation_2s_linear]' src={memoryNew} alt="logo" />
           <p className='text-slate-50' >Welcom to memory game</p>
-          <Link to={`/memorygame`}  className='linkMemory rounded-full font-bold animate-[buttonAnimation_2s_linear] hover:animate-[bounce_1s_infinite]'> 
+          <Link to={`/memory/game`}  className='linkMemory rounded-full font-bold animate-[buttonAnimation_2s_linear] hover:animate-[bounce_1s_infinite]'> 
             Start 
           </Link>
         </div>
